76df947e31e65e2416fef57c2c3f8bdeb3dc0e86
Yanik Ammann <confused@ant.lgbt>
2025-12-18 17:06:12 +0100

    fix(worker): update to new prisma adapter

--- _worker/prisma.ts
+++ _worker/prisma.ts
@@ -1,2 +1,6 @@
-import { PrismaClient } from "@prisma/client"
-export default new PrismaClient()
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "src/generated/prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export default new PrismaClient({ adapter });
